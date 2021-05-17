b6274bcca8409e053ec5fe0c69396b1986ed5d5a
vogelnahuel <vogelnahuel@gmail.com>
2021-05-17 08:58:05 -0300

    creacion de login y restauracion

--- src/pages/login/dashboard/DashBoard.js
+++ src/pages/login/dashboard/DashBoard.js
@@ -2,35 +2,88 @@ import React from 'react'
 import Button from '../../../component/Button/Button'
 import canguro from "../../../assets/login/Canguro.svg";
 import pickersLogo from "../../../assets/login/PickersLogo.svg";
+import {useForm} from '../../../hooks/useForm.js'
+import Swal  from 'sweetalert2'
 
-function DashBoard() {
-    let token =localStorage.getItem("token")
-    console.log(token)
+export const  DashBoard = () => {
+    //let token =localStorage.getItem("token")
+   // console.log(token)
+
+    const [formValues,handleInputBlur,handleInputChange] = useForm({
+      mail:'',
+      errorMail:true,
+      errorMsg:'',
+    });
+  
+
+    const {mail,errorMail,errorMsg} = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(mail!=='' && errorMail!==true){
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Revise las intrucciones de su mail',
+        showConfirmButton: false,
+        timer: 3000
+      })
+    }
+  }
+
     return(
-        <>
-        <div className="containerPrincipal">
+      <>
+      <div className="containerPrincipal">
         <div className="containerSecundario">
+          <div className="arriba2">
           <div className="logo">
-            <img src={pickersLogo}></img>
+            <img src={pickersLogo} alt="PickersLogo"></img>
           </div>
-          <form className="form" onSubmit={""}>
+          <form className="form" onSubmit={handleSubmit}>
             <div className="form-group">
+              <br />
               
-              <input type="password" className="input" name="password" placeholder="Email" onChage={""}/>
-              
+              <input 
+               type="mail" 
+               className="input" 
+               name="mail" 
+               placeholder="Email"
+               
+               onBlur={handleInputBlur}
+               onChange={handleInputChange}
+               value={mail}
+               />
+                {
+                errorMail ? <div className="errorsContainer">
+                 <p className="errors"> {errorMsg}  </p>
+                 </div>:<></>
+               }
+              <br />
               <div className="buttonContainer">
-              <Button className="btn btn-outline-primary button_ mt-5" type="submit" name="button" onClick={""} >Iniciar</Button>
-              </div>
               
+              <Button 
+              className="btn btn-outline-primary button_ mt-5" 
+              type="submit" 
+              name="button" 
+               >Enviar correo
+               </Button>
+              </div>
+             
+            
             </div>
           </form>
+          </div>
+         
+           <img className="img-fluid correcion" src={canguro} alt="pickersFooter"></img>
+          
+         
         </div>
+          
+         
       </div>
-            <div className="canguro"> 
-                <img src={canguro}></img>
-            </div>
-
-        </>
+         
+  
+      </>
   
     )
 }
