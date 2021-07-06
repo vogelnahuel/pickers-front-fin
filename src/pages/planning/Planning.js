3f64d33651b0eda9711af3c497d6bfcee41e3ff0
vogelnahuel <vogelnahuel@gmail.com>
2021-07-06 15:13:56 -0300

    paginas adicionales con nav

--- src/pages/planning/Planning.js
+++ src/pages/planning/Planning.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Header} from '../../component/admin/Header/Header'
+import {Nav} from '../../component/admin/Nav/Nav'
 
 export const Planning = () => {
     return (
-        <div>
+        <div className="background-Grey">
+            
+            <Header/>
+          <div className="mainContainerFlex">  
+                <Nav/>
+                
+              
+                        
+            </div>
             
         </div>
     )
