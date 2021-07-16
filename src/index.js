6bcdb86abf3afea1e4e95220eeb2a54cd424e34a
Andrés Tarrío <atarrio@pickit.net>
2021-07-16 12:40:48 +0000

    index.js edited online with Bitbucket

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import Routes from './routes/Routes';
 import "./index.css"
 
+import { createBrowserHistory } from 'history'
 
+const replaceHashPath = () => {
+  const history = createBrowserHistory()
+  const hash = history.location.hash
+  if (hash) {
+    const path = hash.replace(/^#/, '')
+    if (path) {
+      history.replace(path)
+    }
+  }
+}
+replaceHashPath()
 
 ReactDOM.render(
   <React.StrictMode>
