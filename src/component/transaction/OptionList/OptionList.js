a2fbb4916d91fab6dc8bf118a2d4f19a04836911
vogelnahuel <vogelnahuel@gmail.com>
2021-08-06 10:41:50 -0300

    cambios menores

--- src/component/transaction/OptionList/OptionList.js
+++ src/component/transaction/OptionList/OptionList.js
@@ -16,12 +16,12 @@ export const OptionList = (props) => {
     //const dispatch = useDispatch()
     const FilterSelectedTransaction = props.FilterSelectedTransaction
     
-    const [history, sethistory] = useState(false)
+    const [history, sethistory] = useState(true)
     const [reasonCancel, setreasonCancel] = useState(false);
     const [reasonId, setreasonId] = useState({})
     const [reasonCancelConfirm, setreasonCancelConfirm] = useState(false);
     const [finishModal, setfinishModal] = useState(false);
-    const [dniFinish, setdniFinish] = useState(true)
+    const [dniFinish, setdniFinish] = useState(false)
    
 
     const handleClickCancel = (e) => {
