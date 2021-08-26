5f1b0d2544966a45d4d7dbdc8557440a57b67bff
vogelnahuel <vogelnahuel@gmail.com>
2021-08-26 09:56:54 -0300

    funciona local

--- src/utils/queryParams/parseQueryParams.js
+++ src/utils/queryParams/parseQueryParams.js
@@ -1,5 +1,8 @@
 function parseQueryParams(query) {
-  const queryArray = query.split('?')[1].split('&');
+  let queryArray="";
+  if(query)
+  queryArray= query.split('?')[1].split('&');
+
   let queryParams = {};
   for (let i = 0; i < queryArray.length; i++) {
     const [key, val] = queryArray[i].split('=');
