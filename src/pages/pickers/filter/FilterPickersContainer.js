cea9fcfeffab0d0d92b6021993275757939b5f1f
vogelnahuel <vogelnahuel@gmail.com>
2021-09-21 12:53:52 -0300

    cambios hablados con tomas de wording

--- src/pages/pickers/filter/FilterPickersContainer.js
+++ src/pages/pickers/filter/FilterPickersContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import {connect} from "react-redux";
 import {actions as pendingUserActions, selectors as pendingUserSelectors} from "reducers/pickers";
 import {FilterPickers} from "pages/pickers/filter/FilterPickers";
