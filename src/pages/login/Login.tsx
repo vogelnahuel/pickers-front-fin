c2a8073e3e2a50c5c403ccec2cc6f7974dd51550
Martin <syndeh@MacBook-Pro-de-Martin.local>
2021-09-24 18:01:13 -0300

    clean

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Login.scss";
+import pickersLogo from "./../../assets/login/PickersLogo.svg";
+import canguro from "./../../assets/login/Canguro.svg";
+import { Link } from "react-router-dom";
+import LoginInterface from "./types";
+import { Form, Field } from "react-final-form";
+import { Input } from "component/inputs/Input";
+import useValidationSchema from "hooks/useValidationSchema";
+import NotificationModal from "component/modal/NotificationModal";
+
+const LoginNew: React.FC<LoginInterface> = ({
+  postLogin,
+  validationSchema,
+  isFetching,
+}): JSX.Element => {
+  return (
+    <div className="background-login">
+      <img src={pickersLogo} className="pickersLogo_login" alt="" />
+      <Form
+        onSubmit={postLogin}
+        validate={useValidationSchema(validationSchema)}
+      >
+        {({ handleSubmit }) => (
+          <form className="login-form" onSubmit={handleSubmit}>
+            <Field
+              type="text"
+              name="email"
+              label="Usuario"
+              className="Admin-Pickers-input"
+              component={Input}
+              maxLength={50}
+            />
+
+            <Field
+              type="password"
+              name="password"
+              label="Password"
+              className="Admin-Pickers-input"
+              component={Input}
+            />
+
+            <button
+              className="login-button animation"
+              disabled={isFetching}
+              type="submit"
+              name="button"
+            >
+              <p className="login-init ">Iniciar sesión</p>
+            </button>
+          </form>
+        )}
+      </Form>
+      <div className="container-login-a">
+        <Link className="login-a" to={"/restore"}>
+          ¿Olvidaste tu contraseña?
+        </Link>
+      </div>
+      <img className="login-img-footer" src={canguro} alt="" />
+      <NotificationModal />
+    </div>
+  );
+};
+export default LoginNew;
