1836d1aee6f473fd5fc8b9f5989ff662023ab925
Martin <syndeh@MacBook-Pro-de-Martin.local>
2021-09-27 14:31:10 -0300

    PIC-906 validacion mayusculas en email

--- src/pages/pickers/filter/FilterPickersContainer.js
+++ src/pages/pickers/filter/FilterPickersContainer.js
@@ -25,9 +25,8 @@ const FilterPickersContainer = (props) => {
             return yup.object({
                 name: yup.string().matches(VALIDATION_REGEX.expName,"No se admiten números o caracteres especiales"),
                 identificationNumber:yup.string().matches(VALIDATION_REGEX.expIdentificationNumber,"No se admiten letras o caracteres especiales"),
-                email:yup.string().email("El formato del correo es inválido").matches(VALIDATION_REGEX.regEmail,"El formato del correo es inválido")
+                email:yup.string().matches(VALIDATION_REGEX.regEmail,"El formato del pepe es inválido")
             })
-
         });
     return (
         <FilterPickers
