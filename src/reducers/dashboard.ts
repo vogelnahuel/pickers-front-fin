f9bc1c6673fd102c67b4e544e2b238e1ad811079
Martin <syndeh@MacBook-Pro-de-Martin.local>
2021-11-12 12:55:43 -0300

    fix dashboard toolkit

--- src/reducers/dashboard.ts
+++ src/reducers/dashboard.ts
@@ -1,18 +1,11 @@
-import { AnyAction, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { isRequestAction, isResponseAction } from "reducers";
 import { RootState } from "store";
 import { DashboardState } from "./types/dashboard";
 
 export const initialState: DashboardState = {
   fetching: false,
   dashboard: {},
-};
-
-const isRequestAction = (action: AnyAction) => {
-  return action.type.endsWith("Request");
-};
-
-const isResponseAction = (action: AnyAction) => {
-  return action.type.endsWith("Success") || action.type.endsWith("Error");
 };
 
 export const dashboardSlice = createSlice({
@@ -36,12 +29,8 @@ export const dashboardSlice = createSlice({
     }),
 });
 
-
-// Se exporta el reducer/slice para asociarlo en la creación del store
-export default dashboardSlice.reducer;
-
-// Selector del slice "pickers"
 export const dashboardSelector = (state: RootState) => state.dashboard;
 
-// Se exportan todas las acciones
 export const actions = dashboardSlice.actions;
+
+export default dashboardSlice.reducer;
