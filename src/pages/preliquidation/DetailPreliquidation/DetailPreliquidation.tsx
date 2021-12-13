2d8ab2221da2e2949053390c8500dd14aa870f85
Maxi <maximiliano.caseres@virtualdreams.io>
2021-12-13 09:27:38 -0300

    PIC-1298: merge

--- src/pages/preliquidation/DetailPreliquidation/DetailPreliquidation.tsx
+++ src/pages/preliquidation/DetailPreliquidation/DetailPreliquidation.tsx
@@ -1,6 +1,6 @@
 import { Header } from "component/admin/Header/Header";
 import Nav from "component/admin/Nav/Nav";
-import {TabControler} from "component/admin/TabControler/TabControler";
+import { TabControler } from "component/admin/TabControler/TabControler";
 import { PdfViewer } from "component/pdf-viewer/PdfViewer";
 import React from "react";
 import deletePDF from "../../../assets/preli/deletePDF.svg";
@@ -19,14 +19,12 @@ import { DetailPreliquidationPropsType } from "./types";
 
 import i18next from "i18next";
 
-
-
 export const DetailPreliquidation: React.FC<DetailPreliquidationPropsType> = ({
   isFetching,
-  handleClickBack
+  handleClickBack,
 }): JSX.Element => {
   const actualPage = true;
- 
+
   const tabs = [
     {
       title: "Preliquidacion",
@@ -37,47 +35,71 @@ export const DetailPreliquidation: React.FC<DetailPreliquidationPropsType> = ({
       title: "Factura",
       id: "INVOICE",
       icons: { active: invoiceBlue, disable: invoiceBlack },
-    }
+    },
   ];
 
-  
-
   return (
-
     <div className="background-Grey">
       <Header />
       <div className="mainContainerFlex">
         <Nav />
         <div className="pending-container">
           <div className="preliquidation-display-flex">
-            <TabControler tabs={tabs} changePage={() => { } } actualPage={"INVOICE"} />
-            <div  className="detail-preliquidation-inline" onClick={()=>handleClickBack()}>
+            <TabControler
+              tabs={tabs}
+              changePage={() => {}}
+              actualPage={"INVOICE"}
+            />
+            <div
+              className="detail-preliquidation-inline"
+              onClick={() => handleClickBack()}
+            >
               <img className="img-goBack" src={back} alt="" />
-              <p className="preliquidation-paragraph-goBack">{i18next.t('detailPreliquidation:label.button.goBack')}</p>
+              <p className="preliquidation-paragraph-goBack">
+                {i18next.t("detailPreliquidation:label.button.goBack")}
+              </p>
             </div>
           </div>
           <div className="mainContainerFlex">
-            <h2 className="subTitle-pending">
-              {/* <TabControler
-                tabs={tabs}
-                changePage={() => {
-                  console.log("pepe");
-                } } actualPage={""}               /> */}
-            </h2>
-          </div >
-         <PdfViewer src={pdfFile}>
-           <img src={deletePDF} alt="" onClick={()=>{console.log("soy el boton de eliminar")}} className="download-PDF"/>
-           <img src={replace} alt="" onClick={()=>{console.log("soy el boton de reemplazar")}} className="download-PDF"/>
-           <img src={download} alt="" onClick={()=>{console.log("soy el boton de descarga")}} className="download-PDF"/>
-         </PdfViewer>
-               
-            <h2 className="detail-preliquidation-h2">{i18next.t('detailPreliquidation:label.subtitle.preliquidationNumber')}</h2>
-            <p className="detail-preliquidation-number">{2201100002}</p>
+            <h2 className="subTitle-pending"></h2>
           </div>
-          {actualPage ? <InvoiceContainer /> : <></>}
+          <PdfViewer src={pdfFile}>
+            <img
+              src={deletePDF}
+              alt=""
+              onClick={() => {
+                console.log("soy el boton de eliminar");
+              }}
+              className="download-PDF"
+            />
+            <img
+              src={replace}
+              alt=""
+              onClick={() => {
+                console.log("soy el boton de reemplazar");
+              }}
+              className="download-PDF"
+            />
+            <img
+              src={download}
+              alt=""
+              onClick={() => {
+                console.log("soy el boton de descarga");
+              }}
+              className="download-PDF"
+            />
+          </PdfViewer>
+
+          <h2 className="detail-preliquidation-h2">
+            {i18next.t(
+              "detailPreliquidation:label.subtitle.preliquidationNumber"
+            )}
+          </h2>
+          <p className="detail-preliquidation-number">{2201100002}</p>
         </div>
-        {isFetching === true ? <div className="modalLoading"></div> : <></>}
+        {actualPage ? <InvoiceContainer /> : <></>}
       </div>
-    // </div>
+      {isFetching === true ? <div className="modalLoading"></div> : <></>}
+    </div>
   );
 };
