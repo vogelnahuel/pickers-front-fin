9644a6f4d3b56a6ac0b208b6b91032b83ba8db3f
jbruno <jbruno@pickit.net>
2022-01-11 18:18:09 -0300

    PIC-1526: Change grid-system columns

--- src/pages/preliquidation/DetailPreliquidation/preliquidation/DetailPreliquidation.tsx
+++ src/pages/preliquidation/DetailPreliquidation/preliquidation/DetailPreliquidation.tsx
@@ -1,23 +1,23 @@
-import {TabControler} from "component/admin/TabControler/TabControler";
+
+import React from "react";
+import { TabControler } from "component/admin/TabControler/TabControler";
 import { TabType } from "component/admin/TabControler/types";
 import Back from "component/back/Back";
 import { Input } from "component/inputs/Input";
-
-
 import i18next from "i18next";
-import React from "react";
 import { Field, Form } from "react-final-form";
-
+import edit from "../../../../assets/preli/edit.svg";
+import { PagesPreliquidationTypes } from "../types";
 import calckBlack from "./../../../../assets/preli/calcBlack.svg";
 import calckBlue from "./../../../../assets/preli/calcBlue.svg";
 import invoiceBlack from "./../../../../assets/preli/invoiceBlack.svg";
 import invoiceBlue from "./../../../../assets/preli/invoiceBlue.svg";
+import "./detailPreliquidation.scss";
 import { DetailPreliquidationPropsType } from "./types";
-import  edit from '../../../../assets/preli/edit.svg'
-import "./detailPreliquidation.scss"
-import { PagesPreliquidationTypes } from "../types";
 
-const tabs:TabType<PagesPreliquidationTypes>[] = [
+
+
+const tabs: TabType<PagesPreliquidationTypes>[] = [
   {
     title: "Preliquidacion",
     id: "preliquidation",
@@ -34,7 +34,7 @@ export const DetailPreliquidation = ({
   changePage,
   handleClickBack,
   actualPage,
-  presettementId
+  presettementId,
 }: DetailPreliquidationPropsType) => {
   return (
     <div>
@@ -62,111 +62,105 @@ export const DetailPreliquidation = ({
             changeValue(state, field, () => value);
           },
         }}
-        
       >
-        {({ invalid, handleSubmit, form, values, dirty }) => (
-          <form className="form-filter-transaction" onSubmit={handleSubmit}>
+        {({ handleSubmit, form, values }) => (
+          <form onSubmit={handleSubmit}>
             <div className="display-filter-transaction">
               <div className="container-detail-preliquidation-form-row">
-                <div className="form-filter-transaction">
-                  <div className="container-detail-preliquidation-form-col-sm-1">
-                    <Field
-                      type="text"
-                      name="status"
-                      label="Estado"
-                      component={Input}
-                      className="Admin-Pickers-input"
-                      language="es"
-                      disabled={true}
-                    />
-                  </div>
-                  <div className="container-detail-preliquidation-form-col-sm-1">
-                    <Field
-                      type="text"
-                      label="Fecha de emisión"
-                      name="emisionDate"
-                      component={Input}
-                      className="Admin-Pickers-input"
-                      language="es"
-                      disabled={true}
-                    />
-                  </div>
-                  <div className="container-detail-preliquidation-form-col-sm-1">
-                    <Field
-                      type="text"
-                      name="fiscalNumber"
-                      label="Identificador fiscal"
-                      component={Input}
-                      className="Admin-Pickers-input"
-                      language="es"
-                      disabled={true}
-                    />
-                  </div>
-                  <div className="container-detail-preliquidation-form-col-sm-1">
-                    <Field
-                      type="text"
-                      label="Razón social"
-                      name="companyName"
-                      component={Input}
-                      className="Admin-Pickers-input"
-                      language="es"
-                      disabled={true}
-                    />
-                  </div>
-                  <div className="container-detail-preliquidation-form-col-sm-1">
-                    <Field
-                      type="text"
-                      name="sapCode"
-                      label="Codigo SAP"
-                      component={Input}
-                      className="Admin-Pickers-input"
-                      language="es"
-                      disabled={true}
-                    />
-                  </div>
+                <div className="container-detail-preliquidation-form-col-sm-5 container-detail-preliquidation-form-col-lg-3">
+                  <Field
+                    type="text"
+                    name="status"
+                    label="Estado"
+                    component={Input}
+                    className="Admin-Pickers-input"
+                    language="es"
+                    disabled={true}
+                  />
                 </div>
-              </div>
-            </div>
-            <div className="detail-preliquidation-container-card">
-              <div className="container-detail-preliquidation-card-row">
-                <div className="container-detail-preliquidation-card-col-sm-2">
-                  <h2>Historial</h2>
-                  <div className="display-filter-transaction">
-                    componente historial
-                  </div>
+                <div className="container-detail-preliquidation-form-col-sm-5 container-detail-preliquidation-form-col-lg-3">
+                  <Field
+                    type="text"
+                    label="Fecha de emisión"
+                    name="emisionDate"
+                    component={Input}
+                    className="Admin-Pickers-input"
+                    language="es"
+                    disabled={true}
+                  />
                 </div>
-                <div className="container-detail-preliquidation-card-col-sm-2">
-                  <div className="display-flex">
-                    <h2>Transacciones</h2>
-                    <div className="container-detail-preliquidation-subtitle-amount">
-                      <img src={edit} alt="" />
-                      <p className="detail-preliquidation-subtitle-amount">
-                        Modificar Monto
-                      </p>
-                    </div>
-                  </div>
-                  <div className="display-filter-preliquidation">
-                  componente Transacciones
-                    <div className="transparent"></div>
-
-                    <div>
-                      <h3 className="table-amount-preliquidation-subtitle">
-                        Total
-                      </h3>
-                      <div className="container-table-amount-preliquidation">
-                        <p>Transacciones</p>
-                        <p>
-                          <b>$300.12</b>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+                <div className="container-detail-preliquidation-form-col-sm-5 container-detail-preliquidation-form-col-lg-3">
+                  <Field
+                    type="text"
+                    name="fiscalNumber"
+                    label="Identificador fiscal"
+                    component={Input}
+                    className="Admin-Pickers-input"
+                    language="es"
+                    disabled={true}
+                  />
+                </div>
+                <div className="container-detail-preliquidation-form-col-sm-5 container-detail-preliquidation-form-col-lg-3">
+                  <Field
+                    type="text"
+                    label="Razón social"
+                    name="companyName"
+                    component={Input}
+                    className="Admin-Pickers-input"
+                    language="es"
+                    disabled={true}
+                  />
+                </div>
+                <div className="container-detail-preliquidation-form-col-sm-5 container-detail-preliquidation-form-col-lg-3">
+                  <Field
+                    type="text"
+                    name="sapCode"
+                    label="Codigo SAP"
+                    component={Input}
+                    className="Admin-Pickers-input"
+                    language="es"
+                    disabled={true}
+                  />
                 </div>
               </div>
             </div>
           </form>
         )}
       </Form>
+      <div className="detail-preliquidation-container-card">
+        <div className="container-detail-preliquidation-card-row">
+          <div className="container-detail-preliquidation-card-col-sm-6 container-detail-preliquidation-card-col-xl-4">
+            <h2>Historial</h2>
+            <div className="display-filter-transaction">
+              componente historial
+            </div>
+          </div>
+          <div className="container-detail-preliquidation-card-col-sm-6 container-detail-preliquidation-card-col-xl-4">
+            <div className="display-flex">
+              <h2>Transacciones</h2>
+              <div className="container-detail-preliquidation-subtitle-amount">
+                <img src={edit} alt="" />
+                <p className="detail-preliquidation-subtitle-amount">
+                  Modificar Monto
+                </p>
+              </div>
+            </div>
+            <div className="display-filter-preliquidation">
+              componente Transacciones
+              <div className="transparent"></div>
+              <div>
+                <h3 className="table-amount-preliquidation-subtitle">Total</h3>
+                <div className="container-table-amount-preliquidation">
+                  <p>Transacciones</p>
+                  <p>
+                    <b>$300.12</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
